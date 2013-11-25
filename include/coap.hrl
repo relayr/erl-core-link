13e9991b9c4399611b6784ded5447e3fae4f0444
Jakub Odias <jodias@proximetry.pl>
2013-11-25 09:18:41 +0100

    CoAP implementation.

--- include/coap.hrl
+++ include/coap.hrl
@@ -13,19 +13,50 @@
 %%------------------------------------------------------------------------------
 %% Include files
 %%------------------------------------------------------------------------------
+-include_lib("prox_misc/include/types.hrl").
 
 %%------------------------------------------------------------------------------
 %% Definitions
 %%------------------------------------------------------------------------------
+-define(COAP_CON_MSG, 0).
+-define(COAP_NON_MSG, 1).
+-define(COAP_ACK_MSG, 2).
+-define(COAP_RST_MSG, 3).
+
+-define(COAP_VERSION, 1).
+
+-define(COAP_MSG_PAYLOAD_MARKER, 16#FF).
 
 %%------------------------------------------------------------------------------
 %% Types
 %%------------------------------------------------------------------------------
+-type coap_version()			:: ?COAP_VERSION.
+-type coap_msg_type()			:: ?COAP_CON_MSG | ?COAP_NON_MSG | ?COAP_ACK_MSG | ?COAP_RST_MSG.
+-type coap_msg_token()			:: binary().
+-type coap_msg_code()			:: non_neg_integer().
+-type coap_msg_class()			:: non_neg_integer().
+-type coap_msg_detail()			:: non_neg_integer().
+-type coap_msg_id()				:: id().
 -type coap_resource_attribute() :: resource_type | interface | size.
 
 %%------------------------------------------------------------------------------
 %% Records
 %%------------------------------------------------------------------------------
+-record(coap_option, {
+	id			:: id(),
+	value		:: binary()
+}).
+
+-record(coap_msg, {
+	version		:: coap_version(),
+	type		:: coap_msg_type(),
+	token		:: coap_msg_token(),
+	code		:: coap_msg_code(),
+	id			:: coap_msg_id(),
+	options		:: [#coap_option{}],
+	payload		:: binary()
+}).
+
 -record(coap_resource, {
 	uri			:: nonempty_string(),
 	attributes	:: [{coap_resource_attribute(), string() | integer()}]
