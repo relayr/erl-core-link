4b773cd3327626c84ab993e143b57b0832846593
Jakub Odias <jodias@proximetry.pl>
2013-12-02 10:12:31 +0100

    CoAP implementation.

--- include/coap.hrl
+++ include/coap.hrl
@@ -13,6 +13,7 @@
 %%------------------------------------------------------------------------------
 %% Include files
 %%------------------------------------------------------------------------------
+-include("../include/coap_options.hrl").
 
 %%------------------------------------------------------------------------------
 %% Definitions
@@ -27,9 +28,55 @@
 -define(COAP_MSG_PAYLOAD_MARKER, 16#FF).
 -define(COAP_MAX_MSG_ID, 65535).
 
+-define(COAP_DEFAULT_PORT, 5683).
+-define(COAPS_DEFAULT_PORT, 5684).
+
+%%------------------------------------------------------------------------------
+%% CoAP Response Codes
+%%------------------------------------------------------------------------------
+% Success 2.xx
+-define(COAP_RSP_CODE_CREATED,					201).
+-define(COAP_RSP_CODE_DELETED,					202).
+-define(COAP_RSP_CODE_VALID,					203).
+-define(COAP_RSP_CODE_CHANGED,					204).
+-define(COAP_RSP_CODE_CONTENT,					205).
+
+% Client Error 4.xx
+-define(COAP_RSP_CODE_BAD_REQUEST,				400).
+-define(COAP_RSP_CODE_UNAUTHORIZED,				401).
+-define(COAP_RSP_CODE_BAD_OPTION,				402).
+-define(COAP_RSP_CODE_FORBIDDEN,				403).
+-define(COAP_RSP_CODE_NOT_FOUND,				404).
+-define(COAP_RSP_CODE_METHOD_NOT_ALLOWED,		405).
+-define(COAP_RSP_CODE_METHOD_NOT_ACCEPTABLE,	406).
+-define(COAP_RSP_CODE_PRECONDITION_FAILED,		412).
+-define(COAP_RSP_CODE_REQUEST_ENTITY_TOO_LARGE,	413).
+-define(COAP_RSP_CODE_UNSUPPORTED_CONTENT_FORMAT,415).
+
+% Server Error 5.xx
+-define(COAP_RSP_CODE_INTERNAL_SERVER_ERROR, 	500).
+-define(COAP_RSP_CODE_NOT_IMPLEMENTED, 			501).
+-define(COAP_RSP_CODE_BAD_GATEWAY,				502).
+-define(COAP_RSP_CODE_SERVICE_UNAVAILABLE,		503).
+-define(COAP_RSP_CODE_GATEWAY_TIMEOUT,			504).
+-define(COAP_RSP_CODE_PROXYING_NOT_SUPPORTED,	505).
+%%------------------------------------------------------------------------------
+	
+%%------------------------------------------------------------------------------
+%% CoAP Content-Formats
+%%------------------------------------------------------------------------------
+-define('CoAP-text/plain',				0).
+-define('CoAP-application/link-format',	40).
+-define('CoAP-application/xml',			41).
+-define('CoAP-application/octet-stream',42).
+-define('CoAP-application/exi',			47).
+-define('CoAP-application/json',		50).
+%%------------------------------------------------------------------------------
+
 %%------------------------------------------------------------------------------
 %% Types
 %%------------------------------------------------------------------------------
+-type coap_protocol()			:: coap | coaps.
 -type coap_version()			:: ?COAP_VERSION.
 -type coap_msg_type()			:: ?COAP_CON_MSG | ?COAP_NON_MSG | ?COAP_ACK_MSG | ?COAP_RST_MSG.
 -type coap_msg_token()			:: binary().
@@ -37,17 +84,12 @@
 -type coap_msg_class()			:: non_neg_integer().
 -type coap_msg_detail()			:: non_neg_integer().
 -type coap_msg_id()				:: non_neg_integer().
--type coap_option_id()			:: non_neg_integer().
+-type coap_uri()				:: nonempty_string().
 -type coap_resource_attribute() :: resource_type | interface | size.
 
 %%------------------------------------------------------------------------------
 %% Records
 %%------------------------------------------------------------------------------
--record(coap_option, {
-	id			:: coap_option_id(),
-	value		:: binary()
-}).
-
 -record(coap_msg, {
 	version		:: coap_version(),
 	type		:: coap_msg_type(),
